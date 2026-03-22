d0bc3888d74cc5474301190cada9e5d9e8419b52
nassima-brina <brinanassima59@gmail.com>
2026-03-22 23:01:49 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 bg-blue-500 text-white text-2xl">
+      Tailwind fonctionne !
+    </div>
+  )
+}
+
+export default App
